b421440f5a6a89ddfbc6b0371f9054251d147bae
OnlySekai <minhtuan25902@gmail.com>
2024-02-16 16:10:09 +0700

    :+1: Kick off project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: '@use "@/assets/_colors.scss" as *;'
+          additionalData: '@use "@/assets/scss/_colors.scss" as *;'
         }
       }
     }
