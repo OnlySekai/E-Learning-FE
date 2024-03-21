3d87f63f7bc6080a739909b8ad11e89d4e72fe00
azoom-nguyen-minh-tuan <nguyen.minh.tuan@azoom.jp>
2024-03-21 15:10:17 +0700

    :+1: Refactor code

--- pages/test.vue
+++ pages/test.vue
@@ -1,39 +1,38 @@
-
 <script setup lang="ts">
-import { LeftCircleOutlined, RightCircleOutlined } from '@ant-design/icons-vue';
-import type {SelectProps} from "ant-design-vue";
+import { type SelectValue } from "ant-design-vue/es/select";
+
 definePageMeta({
   layout: false,
-})
+});
 
-const diem = ref<SelectProps['options']>([
-  { value: '1', label: '2-5' },
-  { value: '2', label: '5-7' },
-  { value: '3', label: '7-8' },
+const score = ref([
+  { value: "1", label: "2-5" },
+  { value: "2", label: "5-7" },
+  { value: "3", label: "7-8" },
 ]);
-const kyThi = ref<SelectProps['options']>([
-  { value: 'jack', label: 'Jack' },
-  { value: 'lucy', label: 'Lucy' },
-  { value: 'tom', label: 'Tom' },
+const period = ref([
+  { value: "jack", label: "Jack" },
+  { value: "lucy", label: "Lucy" },
+  { value: "tom", label: "Tom" },
 ]);
-const ngayThi = ref<SelectProps['options']>([
-  { value: 'jack', label: 'Jack' },
-  { value: 'lucy', label: 'Lucy' },
-  { value: 'tom', label: 'Tom' },
+const testDate = ref([
+  { value: "jack", label: "Jack" },
+  { value: "lucy", label: "Lucy" },
+  { value: "tom", label: "Tom" },
 ]);
-const dangBai = ref<SelectProps['options']>([
-  { value: 'jack', label: 'Jack' },
-  { value: 'lucy', label: 'Lucy' },
-  { value: 'tom', label: 'Tom' },
+const post = ref([
+  { value: "jack", label: "Jack" },
+  { value: "lucy", label: "Lucy" },
+  { value: "tom", label: "Tom" },
 ]);
-const handleChange = (value: string) => {
+const handleChange = (value: SelectValue) => {
   console.log(`selected ${value}`);
 };
 const handleBlur = () => {
-  console.log('blur');
+  console.log("blur");
 };
 const handleFocus = () => {
-  console.log('focus');
+  console.log("focus");
 };
 const filterOption = (input: string, option: any) => {
   return option.value.toLowerCase().indexOf(input.toLowerCase()) >= 0;
@@ -47,7 +46,7 @@ const valueDangBai = ref<string | undefined>(undefined);
 
 <template>
   <div>
-    <NuxtLayout name="test" >
+    <NuxtLayout name="test">
       <a-carousel autoplay arrows>
         <template #prevArrow>
           <div class="custom-slick-arrow" style="left: 10px; z-index: 100">
@@ -66,75 +65,77 @@ const valueDangBai = ref<string | undefined>(undefined);
       </a-carousel>
       <div>
         <p class="tieude">Nhập thông tin để tạo lộ trình cá nhân phù hợp</p>
-        <div class="diem">
+        <div class="score">
           <label>Điểm đầu ra: </label>
           <a-select
-              v-model:value="valueDiem"
-              show-search
-              placeholder="điểm bạn muốn"
-              style="width: 200px"
-              :options="diem"
-              :filter-option="filterOption"
-              @focus="handleFocus"
-              @blur="handleBlur"
-              @change="handleChange"
+            v-model:value="valueDiem"
+            show-search
+            placeholder="điểm bạn muốn"
+            style="width: 200px"
+            :options="score"
+            :filter-option="filterOption"
+            @focus="handleFocus"
+            @blur="handleBlur"
+            @change="handleChange"
           ></a-select>
         </div>
-        <div class="kyThi">
+        <div class="period">
           <label>Chọn kỳ thi: </label>
           <a-select
-              v-model:value="valueKyThi"
-              show-search
-              placeholder="điểm bạn muốn"
-              style="width: 200px"
-              :options="kyThi"
-              :filter-option="filterOption"
-              @focus="handleFocus"
-              @blur="handleBlur"
-              @change="handleChange"
+            v-model:value="valueKyThi"
+            show-search
+            placeholder="điểm bạn muốn"
+            style="width: 200px"
+            :options="period"
+            :filter-option="filterOption"
+            @focus="handleFocus"
+            @blur="handleBlur"
+            @change="handleChange"
           ></a-select>
         </div>
-        <div class="ngayThi">
+        <div class="testDate">
           <label>Bạn còn bao nhiêu ngày trước khi thi: </label>
           <a-select
-              v-model:value="valueNgayThi"
-              show-search
-              placeholder="điểm bạn muốn"
-              style="width: 200px"
-              :options="ngayThi"
-              :filter-option="filterOption"
-              @focus="handleFocus"
-              @blur="handleBlur"
-              @change="handleChange"
+            v-model:value="valueNgayThi"
+            show-search
+            placeholder="điểm bạn muốn"
+            style="width: 200px"
+            :options="testDate"
+            :filter-option="filterOption"
+            @focus="handleFocus"
+            @blur="handleBlur"
+            @change="handleChange"
           ></a-select>
         </div>
-        <div class="dangBai">
+        <div class="post">
           <label>Bạn đã học đến dạng bài nào? </label>
           <a-select
-              v-model:value="valueDangBai"
-              show-search
-              placeholder="điểm bạn muốn"
-              style="width: 200px"
-              :options="dangBai"
-              :filter-option="filterOption"
-              @focus="handleFocus"
-              @blur="handleBlur"
-              @change="handleChange"
+            v-model:value="valueDangBai"
+            show-search
+            placeholder="điểm bạn muốn"
+            style="width: 200px"
+            :options="post"
+            :filter-option="filterOption"
+            @focus="handleFocus"
+            @blur="handleBlur"
+            @change="handleChange"
           ></a-select>
         </div>
       </div>
       <div class="huongDan">
         <p>Hướng dẫn làm bài trắc nghiệm:</p>
-        <p>1. Click vào nút "Bắt đầu làm bài" để tiến hành làm bài thi <br>
-           2. Ở mỗi câu hỏi, chọn đáp án đúng <br>
-           3. Hết thời gian làm bài, hệ thống tự thu bài. Bạn có thể nộp bài trước khi thời gian kết thúc bằng cách ấn vào nút nộp bài.</p>
+        <p>
+          1. Click vào nút "Bắt đầu làm bài" để tiến hành làm bài thi <br />
+          2. Ở mỗi câu hỏi, chọn đáp án đúng <br />
+          3. Hết thời gian làm bài, hệ thống tự thu bài. Bạn có thể nộp bài
+          trước khi thời gian kết thúc bằng cách ấn vào nút nộp bài.
+        </p>
       </div>
       <div>
         <a-button type="primary">Bắt đầu làm bài</a-button>
       </div>
     </NuxtLayout>
   </div>
-
 </template>
 <style scoped>
 /* For demo */
