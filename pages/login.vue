b272ea2a4e67b191d12dcaa46ac9b333db3995aa
Vuong752 <159622340+Vuong752@users.noreply.github.com>
2024-04-18 15:45:09 +0700

    :+1: Init login page

---
+++ pages/login.vue
@@ -0,0 +1,42 @@
+<template>
+  <a-card title="Default size card" style="width: 300px">
+    <a-form>
+      <a-form-item
+        label="Username"
+        name="username"
+        :rules="[{ required: true, message: 'Please input your username!' }]"
+      >
+        <a-input v-model:value="formState.username" />
+      </a-form-item>
+      <a-form-item
+        label="Password"
+        name="password"
+        :rules="[{ required: true, message: 'Please input your password!' }]"
+      >
+        <a-input-password v-model:value="formState.password" />
+      </a-form-item>
+
+      <a-form-item name="remember" :wrapper-col="{ offset: 8, span: 16 }">
+      <a-checkbox v-model:checked="formState.remember">Remember me</a-checkbox>
+    </a-form-item>
+
+    <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
+      <a-button type="primary" html-type="submit">Submit</a-button>
+    </a-form-item>  
+    </a-form>
+  </a-card>
+</template>
+
+<script setup lang="ts">
+const formState = reactive({
+  username: '',
+  password: '',
+  remember: false
+})
+</script>
+
+<style lang="scss" scoped>
+body {
+  background-color: $blue;
+}
+</style>
