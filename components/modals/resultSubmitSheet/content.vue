5cac1609e56d5276a739971cf66cb682c2137f4d
onlysekai <minhtuan25902@gmail.com>
2024-04-19 20:07:49 +0700

    :+1: Refactor modal

---
+++ components/modals/resultSubmitSheet/content.vue
@@ -0,0 +1,23 @@
+<template>
+  <a-flex vertical justify="center" align="center">
+    <img src="/images/logo-congratulation.png" alt="congratulation" />
+    <a-typography-title
+      :level="2"
+      :style="{ textAlign: 'center', color: 'red' }"
+    >
+      Chúc mừng bạn đã hoàn thành bài kiểm tra
+    </a-typography-title>
+    <a-flex gap="large" justify="center" align="center">
+      <a-typography-title :level="3"> Điểm số: {{ score }} </a-typography-title>
+      <a-typography-title :level="3">
+        Số câu đúng: {{ correctAnswers }}/{{ numberQuestions }}
+      </a-typography-title>
+    </a-flex>
+  </a-flex>
+</template>
+
+<script lang="ts" setup>
+const quizStore = useQuizStore()
+const { correctAnswers, score } = quizStore.result || {}
+const numberQuestions = quizStore.questions.length
+</script>
--- composables/modals/resultSubmitSheet.ts
+++ composables/modals/resultSubmitSheet.ts
@@ -1,64 +1,13 @@
+import ModalsResultSubmitSheetContent from '~/components/modals/resultSubmitSheet/content.vue'
 import { CheckCircleOutlined } from '@ant-design/icons-vue'
-import { Flex, TypographyTitle } from 'ant-design-vue'
-import { createVNode } from 'vue'
+import { createVNode, h } from 'vue'
 
 export const openResultSubmitSheet = () => {
-  const quizStore = useQuizStore()
-  const { score, correctAnswers } = quizStore.result || {}
-  const numberQuestions = quizStore.questions.length
   Modal.confirm({
     title: 'Kết quả bài thi',
     width: 600,
     icon: createVNode(CheckCircleOutlined, { style: { color: 'blue' } }),
-    content: h(
-      Flex,
-      { vertical: true, justify: 'center', align: 'center' },
-      () => [
-        h('img', {
-          src: '/images/logo-congratulation.png',
-          alt: 'congratulation',
-        }),
-        h(
-          TypographyTitle,
-          {
-            level: 2,
-            style: {
-              textAlign: 'center',
-              color: 'red',
-            },
-          },
-          () => 'Chúc mừng bạn đã hoàn thành bài kiểm tra'
-        ),
-        h(
-          Flex,
-          {
-            vertical: false,
-            gap: 'large',
-            justify: 'center',
-            align: 'center',
-          },
-          {
-            default: () => [
-              h(
-                TypographyTitle,
-                { level: 3 },
-                {
-                  default: () => `Điểm số: ${score}`,
-                }
-              ),
-              h(
-                TypographyTitle,
-                { level: 3 },
-                {
-                  default: () =>
-                    `Số câu đúng: ${correctAnswers}/${numberQuestions}`,
-                }
-              ),
-            ],
-          }
-        ),
-      ]
-    ),
+    content: h(ModalsResultSubmitSheetContent),
     okText: 'Review lại bài kiểm tra',
     cancelText: 'Thoát',
     onOk: () => {
